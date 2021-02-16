4dfdaeb4b7cffb1bdbf7cd07e37d5807ad077e18
GeorgyVeraxa <georg2118@gmail.com>
2021-02-16 18:11:08 +0300

    1

--- homework.py
+++ homework.py
@@ -42,8 +42,9 @@ class CaloriesCalculator(Calculator):
 
     def get_calories_remained(self):
         if self.today_remained() > 0:
+            today_remained = self.today_remained()
             return ('Сегодня можно съесть что-нибудь ещё, но с общей'
-                    f' калорийностью не более {self.today_remained()} кКал')
+                    f' калорийностью не более {today_remained} кКал')
         return 'Хватит есть!'
 
 
@@ -56,11 +57,12 @@ class CashCalculator(Calculator):
                          'usd': ('USD', self.USD_RATE),
                          'eur': ('Euro', self.EURO_RATE)}
         left = round((self.today_remained() / currency_dict[currency][1]), 2)
+        currency_label = currency_dict[currency][0]
         if self.today_remained() > 0:
-            return f'На сегодня осталось {left} {currency_dict[currency][0]}'
+            return f'На сегодня осталось {left} {currency_label}'
         elif self.today_remained() == 0:
             return 'Денег нет, держись'
         else:
             left_minus = abs(left)
             return ('Денег нет, держись: твой долг - '
-                    f'{left_minus} {currency_dict[currency][0]}')
+                    f'{left_minus} {currency_label}')
